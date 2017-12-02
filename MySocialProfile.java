3e988c3950c90763e36a61372720d9fd3b587314
Luke Karis <luked135@gmail.com>
2017-12-02 15:43:47 -0500

    Update MySocialProfile.java

--- MySocialProfile.java
+++ MySocialProfile.java
@@ -1,1 +1,73 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+/**
+*
+*/
+public class MySocialProfile {
+	
+	private BufferedWriter infoWriter;
+	private FileWriter newFile;
+	
+	public MySocialProfile(String name) { //Ask if it is ok to put this code up in the constructor 
+		try {
+			newFile = new FileWriter(name + ".text");
+			infoWriter = new BufferedWriter(newFile);
+		}
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void writeInfo(String info){
+		try{
+			infoWriter.write(info + "\n");
+			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	
+	}
+
+	public void cleanup(){
+		try{
+			infoWriter.close();
+			newFile.close();
+			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/*
+	public void createFile() {
+		try {
+			FileWriter newFile = new FileWriter(name + ".text");
+			BufferedWriter infoWriter = new BufferedWriter(newFile);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+	
+	public static void main(String[] args) {
+		MySocialProfile person = new MySocialProfile("Professor Tarimo");
+		
+		person.writeInfo("This project" + "\n");
+		person.writeInfo("is tricky");
+		person.cleanup();
+		
+		
+	}
+	
+}
