327d476c1a7f166fab7ab3ee5a0c1fd4ab2284bd
DylanGranger <34193643+DylanGranger@users.noreply.github.com>
2017-12-06 16:10:27 -0500

    Update MySocialProfile.java

--- MySocialProfile.java
+++ MySocialProfile.java
@@ -114,66 +114,6 @@ public class MySocialProfile {
 			}
 	}
 
-public void cleanupWriter(){
-		try{
-			infoWriter.close();
-			newFile.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public void cleanupReader(){
-		try{
-			fileRead.close();
-			infoReader.close();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	public static void main(String[] args) {
-		MySocialProfile person = new MySocialProfile();
-		person.createReadFile();
-		//person.createWriteFile();
-		//person.displayProfile();
-		//person.writeInfo("Testing");
-		person.writeTimeline("I like dogs");
-		person.displayProfile();
-		person.cleanupReader();
-		
-		/*
-		MySocialProfile person2 = new MySocialProfile();
-		
-		person2.createFile();
-		person2.checkInfo();
-		person2.cleanup();
-		
-	
-		person.writeInfo("This project" + "\n");
-		person.writeInfo("is tricky");
-		person.cleanup();
-		*/
-		
-		
-	}
-	
-}
-();
-			String password = infoReader.readLine();
-			
-			return(name.equals(userName) && password.equals(userPassword));
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
 	public void cleanupWriter(){
 		try{
 			infoWriter.close();
@@ -195,65 +135,7 @@ public void cleanupWriter(){
 		}
 		
 	}
-	
-	public void createWriteFile() {
-		try {
-			newFile = new FileWriter("MySocialProfile.txt");
-			infoWriter = new BufferedWriter(newFile);
-			
-		}
-		
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void createReadFile() {
-		try {
-			
-			fileRead = new FileReader("MySocialProfile.txt");
-			infoReader = new BufferedReader(fileRead);
-		}
-		
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
-	public void displayProfile() {
-		try {
-			FileReader fileIn = new FileReader("MySocialProfile.txt");
-			BufferedReader bufReader = new BufferedReader(fileIn);
-			
-			String line = bufReader.readLine();
-			System.out.println("Name: " + line);
-			
-			line = bufReader.readLine();
-			line = bufReader.readLine();
-			System.out.println("Email: " + line);
-
-			line = bufReader.readLine();
-			System.out.println("Class Year: " + line);
-			line = bufReader.readLine();
-
-			while (line != null) {
-				System.out.println(line);
-				line = bufReader.readLine();
-			}
-
-			bufReader.close();
-			fileIn.close();
-		}  catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void postTimeline (String post) {
-		timeline.push(post);
-
-		System.out.println(timeline.top());
-	}
-	
 	public static void main(String[] args) {
 		MySocialProfile person = new MySocialProfile();
 		person.createReadFile();
@@ -279,5 +161,4 @@ public void cleanupWriter(){
 		
 		
 	}
-	
 }
